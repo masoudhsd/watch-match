e7f803f7a0010693310679746feca156b3dff21c
Masoud Hasanzadeh <masout.hasanzadeh@gmail.com>
2023-04-20 01:21:02 +0330

    fix(head): change meta data

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,29 +6,29 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
+          <meta name="description" content="Find your movie to watch." />
           <meta
-            name="description"
-            content="Generate your next Twitter bio in seconds."
+            property="og:site_name"
+            content="https://6440606c2b02bb3eca6fe9ec--fascinating-praline-b465a1.netlify.app/"
           />
-          <meta property="og:site_name" content="twitterbio.com" />
           <meta
             property="og:description"
-            content="Generate your next Twitter bio in seconds."
+            content="Find your movie to watch."
           />
-          <meta property="og:title" content="Twitter Bio Generator" />
+          <meta property="og:title" content="Movie Recommendation app with Chat-GPT-3" />
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Twitter Bio Generator" />
+          <meta name="twitter:title" content="Movie Recommendation app with Chat-GPT-3" />
           <meta
             name="twitter:description"
-            content="Generate your next Twitter bio in seconds."
+            content="Find your movie to watch."
           />
           <meta
             property="og:image"
-            content="https://twitterbio.com/og-image.png"
+            content=""
           />
           <meta
             name="twitter:image"
-            content="https://twitterbio.com/og-image.png"
+            content=""
           />
         </Head>
         <body>
